873c5ed97eeb301aa388cc8f1589e56a590a76b6
Daniel PERALES RIOS <danielperales2202@gmail.com>
2024-09-30 15:56:07 +0200

    stuff

---
+++ constants.py
@@ -0,0 +1,19 @@
+class constants:
+
+    nb_points = 64
+
+    Lx = 2e-3
+
+    Ly = 2e-3
+
+    L_slot = 0.5e-3
+
+    L_coflow = 0.5e-3
+
+    U_slot = 1
+
+    T_slot = 300
+
+    U_coflow = 0.2
+
+    T_coflow = 300
--- main.py
+++ main.py
@@ -2,6 +2,28 @@
 
 Final project for the C5 course
 
-Authors: Samuel HOYOS, George SAIFI, Daniel PERALES RIOS
+Authors: Samuel HOYOS, Georges SAIFI, Daniel PERALES RIOS
 
 """
+
+from constants import constants as c
+import numpy as np
+
+# Mesh definition
+
+x = np.linspace(0, c.Lx, c.nb_points)
+y = np.linspace(0, c.Ly, c.nb_points)
+
+
+# Initial conditions & boundary conditions
+
+u_field = np.zeros((c.nb_points, c.nb_points))
+v_field = u_field
+
+# The index for the first velocity is equal to (c.L_slot/c.Lx) * c.nb_points
+v_field[c.nb_points - 1, 0:int((c.L_slot/c.Lx) * c.nb_points)] = c.U_slot
+v_field[0, 0:int((c.L_slot/c.Lx) * c.nb_points)] = -c.U_slot
+print(v_field)
+
+v_field[0, int((c.L_slot/c.Lx) * c.nb_points) + 1:int((c.L_coflow/c.Lx) * c.nb_points)] = c.U_coflow
+v_field[c.nb_points - 1, int((c.L_slot/c.Lx) * c.nb_points) + 1:int((c.L_coflow/c.Lx) * c.nb_points)] = -c.U_coflow
