0ff34d76b4e777d2d84932fa25a853576bb97e29
Daniel PERALES RIOS <danielperales2202@gmail.com>
2024-10-13 22:08:59 +0200

    Modifying equations

--- equations.py
+++ equations.py
@@ -1,27 +1,27 @@
 # Fluid flow equations
 
 import numpy as np
-from constants import constants as c
+from constants import dt, nb_points, eta, dx, dy
 
 
-def fractional_step(u: np.array, v: np.array, P: np.array, dt = c.dt) -> np.array:     
+def fractional_step(u: np.array, v: np.array, P: np.array, dt = dt) -> np.array:     
 
     # Use slices for i and j instead of a for loop for vectorization
     for i in range(0, 1):
-        for j  in range(0, c.nb_points):
+        for j  in range(0, nb_points):
             us= u[i,j] - dt * (u[i, j] * (derivative_x(u, u, i, j) + v[i, j] * derivative_y(u, v, i, j)))
     
-    uss = us + dt * c.eta()
+    uss = us + dt * eta()
 
 
-def derivative_x(u:np.array, v:np.array, i:int, j:int, dx = c.dx) -> np.array: 
+def derivative_x(u:np.array, v:np.array, i:int, j:int, dx = dx) -> np.array: 
     return (u[i, j] - u[i - 1, j]) / dx if u[i, j] > 0 else (u[i + 1, j] - u[i, j]) / dx
 
-def derivative_y(u:np.array, v:np.array, i:int, j:int, dy = c.dy) -> np.array: 
+def derivative_y(u:np.array, v:np.array, i:int, j:int, dy = dy) -> np.array: 
     return (u[i, j] - u[i, j - 1]) / dy if v[i, j] > 0 else (u[i, j + 1] - u[i, j]) / dy
 
-def second_centered_x(u:np.array, i:int, j:int, dx = c.dx) -> np.array: 
+def second_centered_x(u:np.array, i:int, j:int, dx = dx) -> np.array: 
     return (u[i + 1, j] - 2 * u[i, j] + u[i - 1, j]) / dx**2
 
-def second_centered_y(u:np.array, i:int, j:int, dy = c.dy) -> np.array: 
+def second_centered_y(u:np.array, i:int, j:int, dy = dy) -> np.array: 
     return (u[i, j + 1] - 2 * u[i, j] + u[i, j - 1]) / dy**2
--- main.py
+++ main.py
@@ -2,28 +2,33 @@
 
 Final project for the C5 course
 
+In this final project the aim is to modelize the combustion of methane in a counterflow configuration
+
 Authors: Samuel HOYOS, Georges SAIFI, Daniel PERALES RIOS
 
 """
 
-from constants import constants as c
+from constants.constants import Lx, Ly, nb_points, L_slot, U_slot, L_coflow, U_coflow, dt
+import equations as eq
 import numpy as np
 
 # Mesh definition
 
-x = np.linspace(0, c.Lx, c.nb_points)
-y = np.linspace(0, c.Ly, c.nb_points)
+x = np.linspace(0, Lx, nb_points)
+y = np.linspace(0, Ly, nb_points)
 
 
 # Initial conditions & boundary conditions
 
-u_field = np.zeros((c.nb_points, c.nb_points))
+u_field = np.zeros((nb_points, nb_points))
 v_field = u_field
 
 # The index for the first velocity is equal to (c.L_slot/c.Lx) * c.nb_points
-v_field[c.nb_points - 1, 0:int((c.L_slot/c.Lx) * c.nb_points)] = c.U_slot
-v_field[0, 0:int((c.L_slot/c.Lx) * c.nb_points)] = -c.U_slot
+v_field[nb_points - 1, 0:int((L_slot/Lx) * nb_points)] = U_slot
+v_field[0, 0:int((L_slot/Lx) * nb_points)] = -U_slot
 
-v_field[0, int((c.L_slot/c.Lx) * c.nb_points) + 1:int((c.L_coflow/c.Lx) * c.nb_points)] = c.U_coflow
-v_field[c.nb_points - 1, int((c.L_slot/c.Lx) * c.nb_points) + 1:int((c.L_coflow/c.Lx) * c.nb_points)] = -c.U_coflow
+v_field[0, int((L_slot/Lx) * nb_points) + 1:int((L_coflow/Lx) * nb_points)] = U_coflow
+v_field[nb_points - 1, int((L_slot/Lx) * nb_points) + 1:int((L_coflow/Lx) * nb_points)] = -U_coflow
+
+print(eq.fractional_step(u_field, v_field, dt))
 
