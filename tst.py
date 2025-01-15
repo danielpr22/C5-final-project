415d52d327997d555f9cdb496ac3794b140cdf69
Daniel PERALES RIOS <danielperales2202@gmail.com>
2025-01-15 14:28:21 +0100

    Modifying the indexes

--- tst.py
+++ tst.py
@@ -83,64 +83,76 @@ Y_n2 = np.zeros((nb_points, nb_points))
 
 # Attention: the sense of the derivatives is inversed!!
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def derivative_x_upwind(vel: np.array, a:np.array, dx=dx) -> np.array:
     dvel_dx = np.zeros_like(vel)
 
     # For interior points, choose upwind direction based on the sign of u
-    for i in range(1, vel.shape[1] - 1): # Skip the boundaries
+    for i in range(1, vel.shape[1] - 2): # Skip the boundaries
         dvel_dx[:, i] = np.where(a[:, i] >= 0, (vel[:, i] - vel[:, i - 1]) / dx, (vel[:, i + 1] - vel[:, i]) / dx)
 
     return dvel_dx
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def derivative_y_upwind(vel: np.array, a:np.array, dy=dy) -> np.array:
     dvel_dy = np.zeros_like(vel)
 
     # For interior points, choose upwind direction based on the sign of vel
-    for i in range(1, vel.shape[0] - 1): 
+    for i in range(1, vel.shape[0] - 2): 
         # Differenciate between positive and negative flow
         dvel_dy[i, :] = np.where(a[i, :] >= 0, (vel[i, :] - vel[i - 1, :]) / dy, (vel[i + 1, :] - vel[i, :]) / dy)
 
     return dvel_dy
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def derivative_x_centered(u: np.array, dx=dx) -> np.array:
     du_dx = np.zeros_like(u)
-    du_dx[:, 1:-1] = (u[:, 2:] - u[:, :-2]) / (2 * dx)  # Central difference
+
+    for i in range(2, nb_points - 2):
+        for j in range(2, nb_points - 2):
+            du_dx[i, j] = (u[i, j+1] - u[i, j-1]) / (2 * dx)  # Central difference
 
     return du_dx
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def derivative_y_centered(u: np.array, dy=dy) -> np.array:
     du_dy = np.zeros_like(u)
-    du_dy[1:-1, :] = (u[2:, :] - u[:-2, :]) / (2 * dy)  # Central difference for interior points
+
+    for i in range(2, nb_points - 2):
+        for j in range(2, nb_points - 2):
+            du_dy[i, j] = (u[i+1, j] - u[i-1, j]) / (2 * dy)  # Central difference for interior points
 
     return du_dy
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def second_centered_x(u: np.array, dx=dx) -> np.array:
 
-    d2u_dx2 = np.zeros_like(u)
-    d2u_dx2[:, 1:-1] = (u[:, 2:] - 2 * u[:, 1:-1] + u[:, :-2]) / (dx ** 2)
+    for i in range(2, nb_points - 2):
+        for j in range(2, nb_points - 2):
+            u_left = u[i, j-1]
+            u_right = u[i, j+1]
+            u[i, j] = (u_right - 2 * u [i, j] + u_left) / dx**2
 
-    return d2u_dx2
+    return u
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def second_centered_y(u: np.array, dy=dy) -> np.array:
 
-    d2u_dy2 = np.zeros_like(u)
-    d2u_dy2[1:-1, :] = (u[2:, :] - 2 * u[1:-1, :] + u[:-2, :]) / (dy ** 2)
+    for i in range(2, nb_points - 2):
+        for j in range(2, nb_points - 2):
+            u_left = u[i-1, j]
+            u_right = u[i+1, j]
+            u[i, j] = (u_right - 2 * u [i, j] + u_left) / dy**2
 
-    return d2u_dy2
+    return u
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def sor(P, f, tolerance_sor=tolerance_sor, max_iter=max_iter, omega=omega):
     """
     Successive Overrelaxation (SOR) method for solving the pressure Poisson equation.
@@ -163,8 +175,8 @@ def sor(P, f, tolerance_sor=tolerance_sor, max_iter=max_iter, omega=omega):
         P_old = P.copy()
         laplacian_P = P.copy()
         
-        for i in range(1, nb_points - 1):
-            for j in range(1, nb_points - 1):
+        for i in range(2, nb_points - 2):
+            for j in range(2, nb_points - 2):
                 laplacian_P[i, j] = (P_old[i+1, j] + P[i-1, j]) / dx**2 + (P_old[i, j+1] + P[i, j-1]) / dy**2
                 
                 # Update P using the SOR formula
@@ -228,6 +240,7 @@ def sor(P, f, tolerance_sor=tolerance_sor, max_iter=max_iter, omega=omega):
 # Plotting and animating the flow and the velocities #
 ######################################################
 
+#@jit(fastmath=True, nopython=True)
 def plot_velocity_fields(u, v, Lx, Ly, nb_points, L_slot, L_coflow, save_path=None):
     """
     Plot the velocity fields u and v using colormaps, accounting for matrix indexing.
@@ -293,6 +306,7 @@ def plot_velocity_fields(u, v, Lx, Ly, nb_points, L_slot, L_coflow, save_path=No
 
     plt.show()
 
+#@jit(fastmath=True, nopython=True)
 def animate_flow_evolution(
     u_history,
     v_history,
@@ -415,7 +429,7 @@ v_history = []
 strain_rate_history = []
 Y_n2_history = []
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def boundary_conditions(u, v, Y_n2):
     # Boundary conditions for the velocity field
     u[:, 1] = 0
@@ -439,7 +453,7 @@ def boundary_conditions(u, v, Y_n2):
     return u, v, Y_n2
 
 
-@jit(fastmath=True, nopython=True)
+#@jit(fastmath=True, nopython=True)
 def system_evolution_kernel(u, v, P, Y_n2):
     
     u, v, Y_n2 = boundary_conditions(u, v, Y_n2)
