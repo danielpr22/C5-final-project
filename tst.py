ae5d2954d07ea8376df7daf946e2bbc3a8b45352
Daniel PERALES RIOS <danielperales2202@gmail.com>
2025-01-16 03:48:34 +0100

    Last running version

--- tst.py
+++ tst.py
@@ -179,14 +179,14 @@ def sor(P, f, tolerance_sor=tolerance_sor, max_iter=max_iter, omega=omega):
 
     for _ in range(max_iter):
         P_old = P.copy()
-        laplacian_P = P.copy()
+        laplacian_P = np.zeros_like(P)
         
         for i in range(1, nb_points - 1):
             for j in range(1, nb_points - 1):
-                laplacian_P[i, j] = (P_old[i+1, j] + P[i-1, j]) / dx**2 + (P_old[i, j+1] + P[i, j-1]) / dy**2
+                laplacian_P[i, j] = (P[i+1, j] + P[i-1, j]) / dx**2 + (P[i, j+1] + P[i, j-1]) / dy**2
                 
                 # Update P using the SOR formula
-                P[i, j] = (1 - omega) * P_old[i, j] + (omega / coef) * (laplacian_P[i, j] - f[i, j])
+                P[i, j] = (1 - omega) * P[i, j] + (omega / coef) * (laplacian_P[i, j] - f[i, j])
         
         # Compute the residual to check for convergence
         residual = np.linalg.norm(P - P_old, ord=2)
@@ -446,8 +446,8 @@ def boundary_conditions(u, v, Y_n2):
     v[:, 0] = 0 # Left wall
     v[0, 1 : int(L_slot / Lx * nb_points) + 1] = -U_slot # Speed at the top of the "flow" region (Methane)
     v[-1, 1 : int(L_slot / Lx * nb_points) + 1] = U_slot  # Speed at the bottom of the "flow" region (Air)
-    v[0, int(L_slot / Lx * nb_points) : int((L_slot + L_coflow) / Lx * nb_points)] = -U_coflow  # Speed at the top of the "coflow" region (Nitrogen)
-    v[-1, int(L_slot / Lx * nb_points) : int((L_slot + L_coflow) / Lx * nb_points)] = U_coflow # Speed at the bottom of the "coflow" region (Nitrogen)
+    v[0, int(L_slot / Lx * nb_points) + 1 : int((L_slot + L_coflow) / Lx * nb_points)] = -U_coflow  # Speed at the top of the "coflow" region (Nitrogen)
+    v[-1, int(L_slot / Lx * nb_points) + 1 : int((L_slot + L_coflow) / Lx * nb_points)] = U_coflow # Speed at the bottom of the "coflow" region (Nitrogen)
     v[0, int((L_slot + L_coflow) / Lx * nb_points) : nb_points] = 0 # For the top non-slipping wall
     v[-1, int((L_slot + L_coflow) / Lx * nb_points) : nb_points] = 0  # For the bottom non-slipping wall 
     v[:, 1] = v[:, 2] # dv/dx = 0 at the left wall
@@ -459,48 +459,55 @@ def boundary_conditions(u, v, Y_n2):
 
     return u, v, Y_n2
 
+u, v, Y_n2 = boundary_conditions(u, v, Y_n2)
 
 @jit(fastmath=True, nopython=True)
 def system_evolution_kernel(u, v, P, Y_n2):
     
-    u, v, Y_n2 = boundary_conditions(u, v, Y_n2)
+    #u, v, Y_n2 = boundary_conditions(u, v, Y_n2)
 
     # Step 1
-    u_star = u - dt * (u * derivative_x_centered(u) + v * derivative_y_centered(u))
-    v_star = v - dt * (u * derivative_x_centered(v) + v * derivative_y_centered(v))
+    u_star = u + dt * (u * derivative_x_centered(u) + v * derivative_y_centered(u)) + nu * dt * (second_centered_x(u) + second_centered_y(u))
+    v_star = v + dt * (u * derivative_x_centered(v) + v * derivative_y_centered(v)) + nu * dt * (second_centered_x(v) + second_centered_y(v))
     
     # Species transport 
     Y_n2_star = Y_n2 - dt * (u * derivative_x_centered(Y_n2) + v * derivative_y_centered(Y_n2))
 
-    u_star, v_star, Y_n2_star = boundary_conditions(u_star, v_star, Y_n2_star)
+    #u_star, v_star, Y_n2_star = boundary_conditions(u_star, v_star, Y_n2_star)
 
     # Step 2
     u_double_star = u_star + dt * (nu * (second_centered_x(u_star) + second_centered_y(u_star)))
     v_double_star = v_star + dt * (nu * (second_centered_x(v_star) + second_centered_y(v_star)))
     Y_n2_double_star = Y_n2_star + dt * (nu * (second_centered_x(Y_n2_star) + second_centered_y(Y_n2_star)))
 
-    u_double_star, v_double_star, Y_n2_double_star = boundary_conditions(u_double_star, v_double_star, Y_n2_double_star)
+    #u_double_star, v_double_star, Y_n2_double_star = boundary_conditions(u_double_star, v_double_star, Y_n2_double_star)
 
-    P[:, 0] = 0
-    P[:, 1] = P[:, 2] # dP/dx = 0 at the left wall
-    P[0, 1:] = P[1, 1:] # dP/dy = 0 at the top wall
-    P[-1, 1:] = P[-2, 1:] # dP/dy = 0 at the bottom wall
-    P[:, -1] = 0 # P = 0 at the right free limit
+    # P[:, 0] = 0
+    # P[:, 1] = P[:, 2] # dP/dx = 0 at the left wall
+    # P[0, 1:] = P[1, 1:] # dP/dy = 0 at the top wall
+    # P[-1, 1:] = P[-2, 1:] # dP/dy = 0 at the bottom wall
+    # P[:, -1] = 0 # P = 0 at the right free limit
 
     # Step 3 (P is updated)
     P = sor(P, f=rho / dt * (derivative_x_centered(u_double_star) + derivative_y_centered(v_double_star)))
 
-    P[:, 0] = 0
-    P[:, 1] = P[:, 2] # dP/dx = 0 at the left wall
-    P[0, 1:] = P[1, 1:] # dP/dy = 0 at the top wall
-    P[-1, 1:] = P[-2, 1:] # dP/dy = 0 at the bottom wall
-    P[:, -1] = 0 # P = 0 at the right free limit
+    # P[:, 0] = 0
+    # P[:, 1] = P[:, 2] # dP/dx = 0 at the left wall
+    # P[0, 1:] = P[1, 1:] # dP/dy = 0 at the top wall
+    # P[-1, 1:] = P[-2, 1:] # dP/dy = 0 at the bottom wall
+    # P[:, -1] = 0 # P = 0 at the right free limit
    
     # Step 4
     u_new = u_double_star - dt / rho * derivative_x_centered(P)
     v_new = v_double_star - dt / rho * derivative_y_centered(P)
 
     u_new, v_new, Y_n2_double_star = boundary_conditions(u_new, v_new, Y_n2_double_star)
+
+    P[:, 0] = 0
+    P[:, 1] = P[:, 2] # dP/dx = 0 at the left wall
+    P[0, 1:] = P[1, 1:] # dP/dy = 0 at the top wall
+    P[-1, 1:] = P[-2, 1:] # dP/dy = 0 at the bottom wall
+    P[:, -1] = 0 # P = 0 at the right free limit
 
     return u_new, v_new, P, Y_n2_double_star
 
